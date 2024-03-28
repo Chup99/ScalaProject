ecb5ca82998dd3f842b1a33875472018ddb46972
chup <sherminechua@hotmail.com>
2024-03-29 01:19:15 +0800

    checked for correct output

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # ScalaProject
 
+Local package versions:
+Java: jdk22
+sbt: 1.9.8
+Hadoop: 3.3.0
+Spark: 3.5.1
+Scala: 2.13.12
+
+commands to run:
+
+sbt package
 
 C:\Spark\bin\spark-submit --class TopItemsByLocation --master local[*] target/scala-2.13/topitemsbylocation_2.13-0.1.0-SNAPSHOT.jar datasets/sample_dataset_a.parquet datasets/sample_dataset_b.parquet output/ 10
--- src/main/scala/TopItemsByLocation.scala
+++ src/main/scala/TopItemsByLocation.scala
@@ -42,6 +42,8 @@ object TopItemsByLocation {
     val deduplicatedRDD = rddA.map(row => (row._2, row)).reduceByKey((row1, row2) => row1).map(_._2)
     
     val rddWithItemCount = getItemCount(deduplicatedRDD)
+    val first5Rows = rddWithItemCount.take(10)
+    first5Rows.foreach(println)
     val topXItemsByLocation = getItemRanks(rddWithItemCount, topX)
     
     // Convert DataFrame B to RDD
@@ -50,12 +52,12 @@ object TopItemsByLocation {
     
     val outputSchema = StructType(Seq(
       StructField("geographical_location", StringType, nullable = true),
-      StructField("item_name", StringType, nullable = true),
-      StructField("item_rank", StringType, nullable = true)
+      StructField("item_rank", StringType, nullable = true),
+      StructField("item_name", StringType, nullable = true)
     ))
     // Convert rankedItemsWithLocation RDD to DataFrame
     val resultWithLocationDF = spark.createDataFrame(rankedItemsWithLocation.map {
-      case (geographical_location, item_name, item_rank) => Row(geographical_location, item_rank, item_name )
+      case (geographical_location, item_name, item_rank) => Row(geographical_location, item_rank, item_name)
     }, outputSchema)
     // Write result to output path
     resultWithLocationDF.write.mode("overwrite").parquet(outputPath)
@@ -63,45 +65,39 @@ object TopItemsByLocation {
     // Stop SparkSession
     spark.stop()
   }
-  def getItemCount(deduplicatedRDD: RDD[(Long, Long, String)]): RDD[(Long, Long, String, Int)] = {
+  def getItemCount(deduplicatedRDD: RDD[(Long, Long, String)]): RDD[(Long, String, Int)] = {
      // Map each row in deduplicatedRDD to a tuple of ((geographical_location_oid, item_name), 1)
-    val locationItemCounts = deduplicatedRDD.map(row => ((row._1, row._3), 1))
+    val locationItemCounts = deduplicatedRDD.map { case (geographic_id, _, item_name) => ((geographic_id, item_name), 1) }
 
     // Reduce by key to get the count of each item at each location
     val itemCounts = locationItemCounts.reduceByKey(_ + _)
-
-    // Add item_count column to the RDD
-    val rddWithItemCount = deduplicatedRDD.map(row => ((row._1, row._3), row)).join(itemCounts)
-                                          .map{ case ((location, item), (row, count)) => (location, row._2, item, count) }
+    
+    // Rearrange the data to have geographic_id as the key
+    val rddWithItemCount = itemCounts.map { case ((geographic_id, item_name), count) => (geographic_id, item_name, count) }
     rddWithItemCount
   }
 
-  def getItemRanks(rddWithItemCount: RDD[(Long, Long, String, Int)], X: Int): RDD[(Long, Long, String, Int, String)]={
-    // Map each row in rddWithItemCount to ((geographical_location_oid, item_name), (item_count, row))
-    val locationItemCounts = rddWithItemCount.map(row => ((row._1, row._3), (row._4, row)))
+  def getItemRanks(rddWithItemCount: RDD[(Long, String, Int)], X: Int): RDD[(Long, String, String)]={
+    // Map each row in rddWithItemCount to ((geographical_location_oid), (item_name, item_count))
+    val locationItemCounts = rddWithItemCount.map(row => ((row._1), (row._2, row._3)))
 
     // Group by location and sort the items by count in descending order
-    val groupedLocations = locationItemCounts.groupByKey().mapValues(_.toList.sortBy(-_._1))
+    val groupedLocations = locationItemCounts.groupByKey().flatMapValues(_.toList.sortBy(-_._2))
 
-    // Map each group to add rank for each item
-    val rankedItems = groupedLocations.flatMapValues { items =>
-      items.zipWithIndex.map { case ((count, row), rank) => (count, row, rank + 1) }
-    }
-
-    // Filter to keep only top 10 items at each location
-    val top10ItemsPerLocation = rankedItems.filter { case (_, (_, _, rank)) => rank <= X }
+    // Get top X items per location
+    val rankedItems = groupedLocations.groupByKey().flatMapValues(_.zipWithIndex.filter(_._2 < X).map { case ((itemName, itemCount), rank) => (itemName, rank + 1) })
 
     // Extract relevant columns and remove temporary keys
-    val finalResult = top10ItemsPerLocation.map { case ((location, itemName), (count, row, rank)) => (location, row._2, itemName, count, rank.toString()) }
+    val finalResult = rankedItems.map { case (geographicalId, (itemName, rank)) => (geographicalId, itemName, rank.toString()) }
 
     finalResult
   }
 
-  def addLocation(rankedItemsRdd:RDD[(Long, Long, String, Int, String)], locationRdd:RDD[(Long, String)]): RDD[(String, String, String)]={
+  def addLocation(rankedItemsRdd:RDD[(Long, String, String)], locationRdd:RDD[(Long, String)]): RDD[(String, String, String)]={
     // locationRDD is from datasetB, where the first element is geographical_location_oid and the second element is location
 
     // Join ranked RDD with locationRDD
-    val resultWithLocation = rankedItemsRdd.map { case (geographical_location_oid, detection_oid, item_name, count, rank) =>
+    val resultWithLocation = rankedItemsRdd.map { case (geographical_location_oid, item_name, rank) =>
       (geographical_location_oid, ( item_name, rank))
     }.join(locationRdd).map { case (geographical_location_oid,((item_name, rank), location)) =>
       (location, item_name, rank)
